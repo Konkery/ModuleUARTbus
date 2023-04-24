3b2ba8948bf78697f357d53671030c8f2d4786ac
Aleksandr <arrarrat65@gmail.com>
2023-04-24 13:16:00 +0400

    fix:

--- js/module/ClassBaseUARTBus.js
+++ js/module/ClassBaseUARTBus.js
@@ -83,13 +83,11 @@ class ClassBaseUARTBus {
      */
     AddBus(_opt) {
         /*проверить переданные параметры шины на валидность*/
-        print('Gotcha\n');
         if ((typeof (_opt.rx) === 'undefined') || (typeof (_opt.tx) === 'undefined') || (typeof (_opt.baud) === 'undefined')) {
-            print('Gotcha1\n');
            throw new err(this.Pattern, 10);
         }
 
-        if (!(_opt.sda instanceof Pin) || !(_opt.scl instanceof Pin) || !(Number.isInteger(_opt.baud))) {
+        if (!(_opt.rx instanceof Pin) || !(_opt.tx instanceof Pin) || !(Number.isInteger(_opt.baud))) {
             print('Gotcha2\n');
            throw new err(this.Pattern, 20);
         }
